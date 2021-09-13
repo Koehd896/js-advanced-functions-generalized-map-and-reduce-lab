631e15d7375cd6ce26bbd714548563bf3bbd30ad
Raven <ravenkoehler@gmail.com>
2021-09-13 18:05:03 -0400

    commit tree 5c43ba7f75bd11fe9814a0f8979bab9acfb2f5e0

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your functions here
+function map(array, fn) {
+    const newArray = []
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(fn(array[i]));
+    }
+    return newArray;
+}
+
+function reduce(array, fn, start) {
+    let startBoolean = false
+    if(start){startBoolean = true}
+    let value;
+    for(let i = 0; i < array.length; i ++) {
+        if(startBoolean) {
+            value = fn(start, array[i])
+            startBoolean = false;
+        } else if(i === 0) {
+            value = fn(array[i], array[i + 1])
+            i = 1;
+        } else {
+            value = fn(value, array[i]);
+        }
+ 
+    };
+    return value;
+}
